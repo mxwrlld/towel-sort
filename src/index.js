03204d66091c5c9f357cee16da80d8ee98ee73be
mxwrlld <maksim.mixwell@gmail.com>
2020-10-19 22:25:50 +0500

    init: implementation of the towel sorting algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if( toString.call(matrix) != '[object Array]' || matrix.length == 0) {
+    return [];
+  }
+  let arr = [].concat(matrix[0]);
+  for(let i = 1; i < matrix.length; ++i) {
+    if( i % 2 == 0) {
+      arr = arr.concat(matrix[i]);
+    }
+    else {
+      arr = arr.concat(matrix[i].reverse());
+    }
+  }
+  return arr;
 }
